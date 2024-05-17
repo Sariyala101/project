9f6a7f0589154441cf356fd33942cba50a42460f
Sariyala101 <160634019+Sariyala101@users.noreply.github.com>
2024-05-16 23:42:23 -0600

    project
    a started

---
+++ src/data.js
@@ -0,0 +1,61 @@
+const data=[
+    {
+        id:1,
+        info:"Hawa Mahal is a beautiful palace in Jaipur, India. It’s famous for its many small windows. When the wind blows, it passes through these windows and makes the palace cool. Hawa Mahal which means ‘Palace of Winds’. It is made from pink and red sandstone which is why Jaipur is also called the ‘Pink City’. The palace looks like a big crown because it was built for the king and queen. It has five floors but no stairs, only ramps. People from far away come to see this special palace. It’s a very important part of India’s history.",
+        image:"https://images.unsplash.com/photo-1545126178-862cdb469409?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"6700",
+        name: "Jaipur"
+    },
+    {
+        id:2,
+        info:"Goa is a state on the southwestern coast of India. The location was recognized as the Konkan and geographically separated from the Deccan highlands with Western Ghats’ aid. It is encircled by Maharashtra’s Indian state toward the north and Karnataka toward the east and south, with the Arabian Sea shaping its western coast. With terraced hills, red churches, small rooftops, Panaji City stands as the Capital of Goa.",
+        image:"https://images.unsplash.com/photo-1614082242765-7c98ca0f3df3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"10500",
+        name:"Goa"
+
+    },
+{
+        id:3,
+        info:"Indonesia is a fascinating place. Bali is located in the westernmost parts of Indonesia near Sunda Islands. Bali is known as a beautiful island on the East of Java and the major tourist destination of Indonesia. Since the 1980s, a surge in the number of tourists gave rise to tourism businesses in Bali that earn 80% of the economy. Since Bali is a province of Hindu majority, the place has a great culture and is renowned for arts, modern dance, painting, sculpture, and of course, the beaches.",
+        image:"https://images.unsplash.com/photo-1518548419970-58e3b4079ab2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"12000",
+        name:"Bali"
+    },
+{
+        id:4,
+        info:"Visakhapatnam or Vizag is considered to be a port city of the state of Andhra Pradesh. The people living in this city belong to different religions, cultures, and race live with love and harmony. This city is concealed between the hills of the Eastern Ghats facing towards the direction of Bay of Bengal. It is also referred to as the “city of destiny” and “the jewel city of the east coast.”",
+        image:"https://media.istockphoto.com/id/1469116195/photo/rice-terrace-bali-indonesia.jpg?s=1024x1024&w=is&k=20&c=5zMzj9989UBCqUXrYOHnwGWB5_xH2dlWKm4coOqt5Tg=",
+        price:"15000",
+        name:"Vishakhapatnam"
+    },
+{
+        id:5,
+        info:"Agra Fort, also called Red Fort, large 16th-century fortress of red sandstone located on the Yamuna River in the historic city of Agra, west-central Uttar Pradesh, north-central India. It was established by the Mughal emperor Akbar and, in its capacity as both a military base and a royal residence, served as the seat of government when the Mughal capital was in Agra. The structure, a contemporary of Humāyūn’s Tomb in Delhi (about 125 miles [200 km] to the northwest), reflects the architectural grandeur of the Mughal reign in India. The fort complex was designated a UNESCO World Heritage site in 1983.Situated on the site of earlier fortifications, it lies on the right bank of the Yamuna River and is connected to another of Agra’s renowned monuments, the Taj Mahal (downstream, around a bend in the Yamuna), by a swath of parkland and gardens.",
+        image:"https://images.unsplash.com/photo-1585135497273-1a86b09fe70e?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"8000",
+        name:"Agra"
+    },
+   {
+        id:6,
+        info:"Manali is located in India’s northern State Himachal Pradesh. Manali is a most beautiful destination in India. It is located on the banks of the Beas River. It is a most beautiful hill station. Here you can enjoy many adventure activities. You can enjoy trekking, climbing, mountain biking, Paragliding, and skiing. There are many places to explore on this beautiful station. Manali attracts tourist from all over the world.",
+        image:"https://images.unsplash.com/photo-1605649487212-47bdab064df7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"20000",
+        name:"Manali"
+    }, 
+    {
+        id:7,
+        info:"The north Indian city of Varanasi, or Benares, is regarded as sacred by Hindus, Buddhists and Jains. Situated in the fertile Ganga valley and on the banks of the Ganges, Varanasi is a place of pilgrimage for Hindus, who come to bathe in the holy river. Known as the City of Temples and a beacon of culture, Varanasi has produced many renowned writers. It’s also a center of silk weaving, so practice haggling at the chaotic bazaar for some beautiful souvenirs. Rickshaws are the best way to get about.",
+        image: "https://images.unsplash.com/photo-1561359313-0639aad49ca6?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"7500",
+        name: "Varanasi"
+    },
+    {
+        id:8,
+        info:"Capital of Karnataka is Bangalore. Kannada is the most widely spoken language in Karnataka. It is the largest state in South India. The neighbouring states of Karnataka are Goa, Maharashtra, Telangana, Andhra Pradesh, Kerala and Tamil Nadu. It is bound by Arabian Sea to its West. Karnataka has many colleges and universities serving the needs of a large Indian population. The silicon valley of India is located in Karnataka. There are many scientific organisations located in Karnataka. Karnataka is the 6th largest state in India by area and 8th largest state in India by population. There are 31 districts in the state of Karnataka. Karnataka was earlier known as the State of Mysore. Some of the most famous tourist attractions in Karnataka are its beaches, Mysore Palace, Hampi, Badami, Aihole, Pattadakal, Coorg, Shivanasamudra Falls, Jog Falls, different wildlife sanctuaries etc. Karnataka has many Eastward flowing and Westward flowing rivers.",
+        image: "https://images.unsplash.com/photo-1622117655866-8b233395581a?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"17000",
+        name: "Karnataka"
+    }
+
+];
+export default data;
